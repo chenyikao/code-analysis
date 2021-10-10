04a339e91464bd1d929c3b0ba6c6135ec1c3aba3
chenyikao <chenyi.kao@gmail.com>
2021-10-10 08:29:40 +0800

    The first publishing of Fozusoft SMT Solver Elements project for Java
    Common wrapper elements are moved to code-analysis for
    more general reuse.

---
+++ Release/OmpCA.bat
@@ -0,0 +1,3 @@
+"%ProgramFiles%\TextTransformer\bin\tetra_cl.exe" -p SMT_model_to_RED.ttp -s model -t THREADS_MAX.value
+
+"%ProgramFiles%\TextTransformer\bin\tetra_cl.exe" -p parser_v110926_4_Z3.ttp -s c_lu_buggy.c -t c_lu_buggy.d
